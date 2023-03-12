20bee56aaccd7d444c233db81ddc5ad06f938cf8
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-03-12 11:02:38 +0200

    Add the showBooks function
    It loops through the myLibrary array and displays each book on the page.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const authorInput = document.querySelector('.form-author');
 const pagesInput = document.querySelector('.form-page-number');
 const isReadCheckbox = document.querySelector('#read');
 
+const bookCardsSection = document.querySelector('.book-cards-section');
+
 let lastTitle;
 let lastAuthor;
 let lastPagesNum;
@@ -22,10 +24,61 @@ function Book(title, author, pagesNum, isRead) {
   this.isRead = isRead;
 }
 
+// Loops through the myLibrary array and displays each book on the page
+function showBooks() {
+  bookCardsSection.innerHTML = '';
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+
+    const bookTitle = book.title;
+    const bookAuthor = book.author;
+    const bookPages = book.pagesNum;
+    const bookIsRead = book.isRead;
+
+    const bookCardEl = document.createElement('div');
+    bookCardEl.classList.add('book-card');
+    bookCardsSection.appendChild(bookCardEl);
+
+    const infoContainerEl = document.createElement('div');
+    infoContainerEl.classList.add('info-container');
+    bookCardEl.appendChild(infoContainerEl);
+
+    const bookTitleEl = document.createElement('div');
+    bookTitleEl.textContent = `${bookTitle}`;
+    bookTitleEl.classList.add('book-title');
+    infoContainerEl.appendChild(bookTitleEl);
+
+    const bookAuthorEl = document.createElement('div');
+    bookAuthorEl.textContent = `by ${bookAuthor}`;
+    bookAuthorEl.classList.add('book-author');
+    infoContainerEl.appendChild(bookAuthorEl);
+
+    const bookPagesEl = document.createElement('div');
+    bookPagesEl.textContent = `${bookPages} pages`;
+    bookPagesEl.classList.add('book-page-number');
+    infoContainerEl.appendChild(bookPagesEl);
+
+    const readButtonEl = document.createElement('button');
+    if (bookIsRead) {
+      readButtonEl.textContent = 'Read';
+      readButtonEl.classList.add('book-is-read');
+    } else {
+      readButtonEl.textContent = 'Not read';
+      readButtonEl.classList.add('book-not-read');
+    }
+    infoContainerEl.appendChild(readButtonEl);
+
+    const removeButtonEl = document.createElement('button');
+    removeButtonEl.textContent = 'Remove';
+    removeButtonEl.classList.add('remove-button');
+    infoContainerEl.appendChild(removeButtonEl);
+  }
+}
+
 // Adds new book object to the library array based on the users input
 function addBookToLibrary(title, author, pagesNum, isRead) {
   myLibrary.push(new Book(title, author, pagesNum, isRead));
-  console.log(myLibrary);
 }
 
 // Changes the form status
@@ -56,4 +109,5 @@ form.addEventListener('submit', (e) => {
   }
   addBookToLibrary(lastTitle, lastAuthor, lastPagesNum, lastIsRead);
   changeFormStatus();
+  showBooks();
 });
